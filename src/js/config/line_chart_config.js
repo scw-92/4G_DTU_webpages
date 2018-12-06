71f02f605b0732bdf69524cc85311230ed46ede8
scw-92 <1142344150@qq.com>
2018-12-06 16:41:49 +0800

    support line chart

--- src/js/config/line_chart_config.js
+++ src/js/config/line_chart_config.js
@@ -6,43 +6,67 @@ var echarts_option = {
     
     temperature:
     {
-        title: {
-            text: '温度',
+        title : {
+            text : '温度采集',
         },
-        tooltip: {
-            trigger: 'axis'
-        },
-        legend: {
-            data:['最高气温']
+        tooltip : {
+            trigger: 'item',
+            formatter : function (params) {
+                var date = new Date(params.value[0]);
+                data = date.getFullYear() + '-'
+                       + (date.getMonth() + 1) + '-'
+                       + date.getDate() + ' '
+                       + date.getHours() + ':'
+                       + date.getMinutes() + ':'
+                       + date.getSeconds();
+                return data + '<br/>'
+                       + params.value[1];
+            }
         },
         toolbox: {
+            show : true,
+            feature : {
+                dataZoom: { yAxisIndex: 'none'},
+                mark : {show: true},
+                dataView : {show: true, readOnly: false},
+                restore : {show: true},
+                saveAsImage : {show: true}
+                
+            }
+        },
+        dataZoom: {
             show: true,
-            feature: {
-                dataZoom: {
-                    yAxisIndex: 'none'
-                },
-                dataView: {readOnly: false},
-                magicType: {type: ['line', 'bar']},
-                restore: {},
-                saveAsImage: {}
-            }
+            start : 0
         },
-        xAxis:  {
-            type: 'category',
-            boundaryGap: false,
-            data: ['周一','周二','周三','周四','周五','周六','周日']
+        legend : {
+            data : ['温度采集']
         },
-        yAxis: {
-            type: 'value',
-            axisLabel: {
-                formatter: '{value} °C'
-            }
+        grid: {
+            y2: 80
         },
-        series: 
+        xAxis : [
             {
-                name:'气温',
-                type:'line',
-                data:[11, 11, 15, 13, 12, 9, 10],
+                type : 'time',
+                splitNumber:5
+            }
+        ],
+        yAxis : [
+            {
+                type : 'value',
+                axisLabel: {
+                formatter: '{value} °C'
+                }
+            }
+        ],
+        series : [
+            {
+                name: '温度采集',
+                type: 'line',
+                showAllSymbol: true,
+                symbolSize:8,
+                data:[
+
+                        ],
                 markPoint: {
                     data: [
                         {type: 'max', name: '最大值'},
@@ -55,50 +79,73 @@ var echarts_option = {
                     ]
                 }
             }
-        
+        ]
     
     },
 
     air_humidity:
     {
-        title: {
-            text: '空气湿度',
-            subtext: '纯属虚构'
+        title : {
+            text : '空气湿度',
         },
-        tooltip: {
-            trigger: 'axis'
-        },
-        legend: {
-            data:['最高湿度','最低湿度']
+        tooltip : {
+            trigger: 'item',
+            formatter : function (params) {
+                var date = new Date(params.value[0]);
+                data = date.getFullYear() + '-'
+                       + (date.getMonth() + 1) + '-'
+                       + date.getDate() + ' '
+                       + date.getHours() + ':'
+                       + date.getMinutes() + ':'
+                       + date.getSeconds();
+                return data + '<br/>'
+                       + params.value[1];
+            }
         },
         toolbox: {
+            show : true,
+            feature : {
+                dataZoom: { yAxisIndex: 'none'},
+                mark : {show: true},
+                dataView : {show: true, readOnly: false},
+                restore : {show: true},
+                saveAsImage : {show: true}
+                
+            }
+        },
+        dataZoom: {
             show: true,
-            feature: {
-                dataZoom: {
-                    yAxisIndex: 'none'
-                },
-                dataView: {readOnly: false},
-                magicType: {type: ['line', 'bar']},
-                restore: {},
-                saveAsImage: {}
-            }
+            start : 0
         },
-        xAxis:  {
-            type: 'category',
-            boundaryGap: false,
-            data: ['周一','周二','周三','周四','周五','周六','周日']
+        legend : {
+            data : ['空气湿度']
         },
-        yAxis: {
-            type: 'value',
-            axisLabel: {
-                formatter: '{value} RH(%)'
-            }
+        grid: {
+            y2: 80
         },
-        series: 
+        xAxis : [
             {
-                name:'最高湿度',
-                type:'line',
-                data:[11, 11, 15, 13, 12, 13, 10],
+                type : 'time',
+                splitNumber:5
+            }
+        ],
+        yAxis : [
+            {
+                type : 'value',
+                axisLabel: {
+                formatter: '{value} %'
+                }
+            }
+        ],
+        series : [
+            {
+                name: '空气湿度',
+                type: 'line',
+                showAllSymbol: true,
+                symbolSize:8,
+                data:[
+
+                        ],
                 markPoint: {
                     data: [
                         {type: 'max', name: '最大值'},
@@ -111,50 +158,73 @@ var echarts_option = {
                     ]
                 }
             }
-        
+        ]
     
     },
 
     soil_moisture:
         {
-        title: {
-            text: '土壤湿度',
-            subtext: '纯属虚构'
+        title : {
+            text : '土壤湿度',
         },
-        tooltip: {
-            trigger: 'axis'
-        },
-        legend: {
-            data:['最高气温','最低气温']
+        tooltip : {
+            trigger: 'item',
+            formatter : function (params) {
+                var date = new Date(params.value[0]);
+                data = date.getFullYear() + '-'
+                       + (date.getMonth() + 1) + '-'
+                       + date.getDate() + ' '
+                       + date.getHours() + ':'
+                       + date.getMinutes() + ':'
+                       + date.getSeconds();
+                return data + '<br/>'
+                       + params.value[1];
+            }
         },
         toolbox: {
+            show : true,
+            feature : {
+                dataZoom: { yAxisIndex: 'none'},
+                mark : {show: true},
+                dataView : {show: true, readOnly: false},
+                restore : {show: true},
+                saveAsImage : {show: true}
+                
+            }
+        },
+        dataZoom: {
             show: true,
-            feature: {
-                dataZoom: {
-                    yAxisIndex: 'none'
-                },
-                dataView: {readOnly: false},
-                magicType: {type: ['line', 'bar']},
-                restore: {},
-                saveAsImage: {}
-            }
+            start : 0
         },
-        xAxis:  {
-            type: 'category',
-            boundaryGap: false,
-            data: ['周一','周二','周三','周四','周五','周六','周日']
+        legend : {
+            data : ['土壤湿度']
         },
-        yAxis: {
-            type: 'value',
-            axisLabel: {
-                formatter: '{value} °C'
-            }
+        grid: {
+            y2: 80
         },
-        series: 
+        xAxis : [
             {
-                name:'最高气温',
-                type:'line',
-                data:[11, 11, 15, 13, 12, 13, 10],
+                type : 'time',
+                splitNumber:5
+            }
+        ],
+        yAxis : [
+            {
+                type : 'value',
+                axisLabel: {
+                formatter: '{value} %'
+                }
+            }
+        ],
+        series : [
+            {
+                name: '土壤湿度',
+                type: 'line',
+                showAllSymbol: true,
+                symbolSize:8,
+                data:[
+
+                        ],
                 markPoint: {
                     data: [
                         {type: 'max', name: '最大值'},
@@ -167,50 +237,73 @@ var echarts_option = {
                     ]
                 }
             }
-        
+        ]
     
     },
     
         light_intensity:
         {
-        title: {
-            text: '光照强度',
-            subtext: '纯属虚构'
+        title : {
+            text : '光照强度',
         },
-        tooltip: {
-            trigger: 'axis'
-        },
-        legend: {
-            data:['最高气温','最低气温']
+        tooltip : {
+            trigger: 'item',
+            formatter : function (params) {
+                var date = new Date(params.value[0]);
+                data = date.getFullYear() + '-'
+                       + (date.getMonth() + 1) + '-'
+                       + date.getDate() + ' '
+                       + date.getHours() + ':'
+                       + date.getMinutes() + ':'
+                       + date.getSeconds();
+                return data + '<br/>'
+                       + params.value[1];
+            }
         },
         toolbox: {
+            show : true,
+            feature : {
+                dataZoom: { yAxisIndex: 'none'},
+                mark : {show: true},
+                dataView : {show: true, readOnly: false},
+                restore : {show: true},
+                saveAsImage : {show: true}
+                
+            }
+        },
+        dataZoom: {
             show: true,
-            feature: {
-                dataZoom: {
-                    yAxisIndex: 'none'
-                },
-                dataView: {readOnly: false},
-                magicType: {type: ['line', 'bar']},
-                restore: {},
-                saveAsImage: {}
-            }
+            start : 0
         },
-        xAxis:  {
-            type: 'category',
-            boundaryGap: false,
-            data: ['周一','周二','周三','周四','周五','周六','周日']
+        legend : {
+            data : ['光照强度']
         },
-        yAxis: {
-            type: 'value',
-            axisLabel: {
-                formatter: '{value} °C'
-            }
+        grid: {
+            y2: 80
         },
-        series: 
+        xAxis : [
             {
-                name:'最高气温',
-                type:'line',
-                data:[11, 11, 15, 13, 12, 13, 10],
+                type : 'time',
+                splitNumber:5
+            }
+        ],
+        yAxis : [
+            {
+                type : 'value',
+                axisLabel: {
+                formatter: '{value} %'
+                }
+            }
+        ],
+        series : [
+            {
+                name: '光照强度',
+                type: 'line',
+                showAllSymbol: true,
+                symbolSize:8,
+                data:[
+
+                        ],
                 markPoint: {
                     data: [
                         {type: 'max', name: '最大值'},
@@ -223,50 +316,73 @@ var echarts_option = {
                     ]
                 }
             }
-        
+        ]
     
     },
     
         CO2_concentration:
         {
-        title: {
-            text: 'CO2浓度',
-            subtext: '纯属虚构'
+        title : {
+            text : 'CO2浓度',
         },
-        tooltip: {
-            trigger: 'axis'
-        },
-        legend: {
-            data:['最高气温','最低气温']
+        tooltip : {
+            trigger: 'item',
+            formatter : function (params) {
+                var date = new Date(params.value[0]);
+                data = date.getFullYear() + '-'
+                       + (date.getMonth() + 1) + '-'
+                       + date.getDate() + ' '
+                       + date.getHours() + ':'
+                       + date.getMinutes() + ':'
+                       + date.getSeconds();
+                return data + '<br/>'
+                       + params.value[1];
+            }
         },
         toolbox: {
+            show : true,
+            feature : {
+                dataZoom: { yAxisIndex: 'none'},
+                mark : {show: true},
+                dataView : {show: true, readOnly: false},
+                restore : {show: true},
+                saveAsImage : {show: true}
+                
+            }
+        },
+        dataZoom: {
             show: true,
-            feature: {
-                dataZoom: {
-                    yAxisIndex: 'none'
-                },
-                dataView: {readOnly: false},
-                magicType: {type: ['line', 'bar']},
-                restore: {},
-                saveAsImage: {}
-            }
+            start : 0
         },
-        xAxis:  {
-            type: 'category',
-            boundaryGap: false,
-            data: ['周一','周二','周三','周四','周五','周六','周日']
+        legend : {
+            data : ['CO2浓度']
         },
-        yAxis: {
-            type: 'value',
-            axisLabel: {
-                formatter: '{value} °C'
-            }
+        grid: {
+            y2: 80
         },
-        series: 
+        xAxis : [
             {
-                name:'最高气温',
-                type:'line',
-                data:[11, 11, 15, 13, 12, 13, 10],
+                type : 'time',
+                splitNumber:5
+            }
+        ],
+        yAxis : [
+            {
+                type : 'value',
+                axisLabel: {
+                formatter: '{value} %'
+                }
+            }
+        ],
+        series : [
+            {
+                name: 'CO2浓度',
+                type: 'line',
+                showAllSymbol: true,
+                symbolSize:8,
+                data:[
+
+                        ],
                 markPoint: {
                     data: [
                         {type: 'max', name: '最大值'},
@@ -279,50 +395,73 @@ var echarts_option = {
                     ]
                 }
             }
-        
+        ]
     
     },
     
         pressure:
         {
-        title: {
-            text: '压强',
-            subtext: '纯属虚构'
+        title : {
+            text : '大气压强',
         },
-        tooltip: {
-            trigger: 'axis'
-        },
-        legend: {
-            data:['最高气温','最低气温']
+        tooltip : {
+            trigger: 'item',
+            formatter : function (params) {
+                var date = new Date(params.value[0]);
+                data = date.getFullYear() + '-'
+                       + (date.getMonth() + 1) + '-'
+                       + date.getDate() + ' '
+                       + date.getHours() + ':'
+                       + date.getMinutes() + ':'
+                       + date.getSeconds();
+                return data + '<br/>'
+                       + params.value[1];
+            }
         },
         toolbox: {
+            show : true,
+            feature : {
+                dataZoom: { yAxisIndex: 'none'},
+                mark : {show: true},
+                dataView : {show: true, readOnly: false},
+                restore : {show: true},
+                saveAsImage : {show: true}
+                
+            }
+        },
+        dataZoom: {
             show: true,
-            feature: {
-                dataZoom: {
-                    yAxisIndex: 'none'
-                },
-                dataView: {readOnly: false},
-                magicType: {type: ['line', 'bar']},
-                restore: {},
-                saveAsImage: {}
-            }
+            start : 0
         },
-        xAxis:  {
-            type: 'category',
-            boundaryGap: false,
-            data: ['周一','周二','周三','周四','周五','周六','周日']
+        legend : {
+            data : ['大气压强']
         },
-        yAxis: {
-            type: 'value',
-            axisLabel: {
-                formatter: '{value} °C'
-            }
+        grid: {
+            y2: 80
         },
-        series: 
+        xAxis : [
             {
-                name:'最高气温',
-                type:'line',
-                data:[11, 11, 15, 13, 12, 13, 10],
+                type : 'time',
+                splitNumber:5
+            }
+        ],
+        yAxis : [
+            {
+                type : 'value',
+                axisLabel: {
+                formatter: '{value} %'
+                }
+            }
+        ],
+        series : [
+            {
+                name: '大气压强',
+                type: 'line',
+                showAllSymbol: true,
+                symbolSize:8,
+                data:[
+
+                        ],
                 markPoint: {
                     data: [
                         {type: 'max', name: '最大值'},
@@ -335,21 +474,25 @@ var echarts_option = {
                     ]
                 }
             }
-        
+        ]
     
     }
 };
+// 折线图的html元素div的ID = line_names[x]
+// y 轴数据 series["data"][1]
+// x 轴数据 series["data"][0] 注意：x轴数据是个时间类型（DATA）
 var line_configs ={
-        line_names:[
-                    "temperature",
+        line_names:[                                // 折线图对应的div的id名字
+                    "temperature",                  //这些名字既是div的id，也是echarts_option的key值。
                     "air_humidity",
                     "soil_moisture",
                     "light_intensity",
                     "CO2_concentration",
                     "pressure"
                     ],
-        line_style:"sub_line_div",
-        line_parent_div:"line_parent_div",
-        my_echarts_obj:[],
-        my_echarts_value:echarts_option
+        line_style:"sub_line_div",                //折线图对应的div的样式
+        line_parent_div:"line_parent_div",        //折线图对应的div的父容器的id
+        my_echarts_obj:[],                        //echarts对象数组，每一个折线图对应一个echarts对象
+        my_echarts_value:echarts_option,          //折线图对应的图表数据
+        line_disable:[]                           //折线图是否显示（使用）的标志，给定时器请求数据所用
 };
